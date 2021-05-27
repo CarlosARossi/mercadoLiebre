4afa998004495ecb3bf52519927d68c2a192da28
CarlosARossi <carlos91rossi@gmail.com>
2021-05-27 19:50:48 -0300

    Subida inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.set("port",process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => console.log("Server Start in http://localhost:3000")); 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','home.html') )
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','register.html') )
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','login.html') )
+});
